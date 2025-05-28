56c2f694ff8e864b702645c8211279b2416a281d
takhoa13 <takhoa1301@gmail.com>
2025-05-28 14:23:47 +0700

    Update MAF_file_gastric.R

--- MAF_file_gastric.R
+++ MAF_file_gastric.R
@@ -12,7 +12,6 @@ library("survival")
 library("survminer")
 library("RColorBrewer")
 library("gProfileR")
-#library("genefilter")
 library("DESeq2")
 library(pheatmap)
 library(colorRamp2)
@@ -43,7 +42,6 @@ pt.vs.rt <- mafCompare(m1 = maf_lowrisk, m2 = maf_highrisk, m1Name = 'Low risk',
 print(pt.vs.rt)
 pt.vs.rt$results<-pt.vs.rt$results[pt.vs.rt$results$or!='Inf',]
 forestPlot(mafCompareRes = pt.vs.rt, pVal = 0.0001)
-?forestPlot
 
 vc_cols = RColorBrewer::brewer.pal(n = 8, name = 'Spectral')
 names(vc_cols) = c(
@@ -62,19 +60,6 @@ coBarplot(m1 = maf_lowrisk, m2 = maf_highrisk, m1Name = 'Low risk', m2Name = 'Hi
 somaticInteractions(maf = maf_highrisk, top = 10, pvalue = c(0.05, 0.01),showSum = F,showSigSymbols = T,leftMar = 8)
 plotmafSummary(maf = maf_highrisk, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
 
-
-#mafSurvival(maf = maf_lowrisk, genes = 'NEB', time = 'days_to_last_followup', Status = 'vital_status', isTCGA = TRUE)
-
-
 ##### make mutation matrix
 mut_list <- make_mut_matrix(maf = maf, isTCGA   = T, category = "multi")
-
-
-drugInteractions(maf = maf_highrisk, fontSize = 0.75,plotType = 'bar')
-
-
-dev.off()
-library("BSgenome.Hsapiens.UCSC.hg19", quietly = TRUE)
-maf_lowrisk.tnm = trinucleotideMatrix(maf = maf_lowrisk, prefix = 'chr', add = TRUE, ref_genome = "BSgenome.Hsapiens.UCSC.hg19")
-plotApobecDiff(tnm = maf_lowrisk.tnm, maf = maf_lowrisk, pVal = 0.2)
 
